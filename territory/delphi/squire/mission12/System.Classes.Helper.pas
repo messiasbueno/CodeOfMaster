796b5f81430658bc391988f23a5aaeed0cab51d5
Messias <messias.acb@gmail.com>
2019-10-06 14:44:36 -0300

    Delphi - Squire - mission12 - Class Helper

---
+++ territory/delphi/squire/mission12/System.Classes.Helper.pas
@@ -0,0 +1,114 @@
+unit System.Classes.Helper;
+
+interface
+
+uses
+  System.Classes;
+
+type
+  TStringListHelper = class helper for TStringList
+  private
+    procedure ValidToJson;
+  public
+    function IsEmpty: Boolean;
+    function HasDuplicate: Boolean;
+    function toJson: string;
+  end;
+
+implementation
+
+uses
+  System.SysUtils,
+  System.Math,
+  System.JSON;
+
+{ TStringListHelper }
+
+function TStringListHelper.IsEmpty: Boolean;
+begin
+  Result := Self.Count = ZeroValue;
+end;
+
+function TStringListHelper.HasDuplicate: Boolean;
+var
+  sList: TStringList;
+  sItem: string;
+begin
+  sList := TStringList.Create;
+  try
+    sList.Sorted := True;
+    sList.Duplicates := dupIgnore;
+
+    for sItem in Self do
+      sList.Add(sItem);
+
+    Result := sList.Count <> Self.Count;
+  finally
+    sList.Free;
+  end;
+end;
+
+procedure TStringListHelper.ValidToJson;
+var
+  nIndex: Integer;
+begin
+  if (Self.IsEmpty) then
+    raise Exception.Create('List is Empty');
+
+  if (Self.HasDuplicate) then
+    raise Exception.Create('List has not Duplicate item');
+
+  for nIndex := ZeroValue to Pred(Self.Count) do
+  begin
+    if (Self.Names[nIndex] = EmptyStr) or
+       (Self.ValueFromIndex[nIndex] = EmptyStr) then
+      raise Exception.Create('List is invalid');
+  end;
+end;
+
+function TStringListHelper.toJson: string;
+var
+  oJSON: TJSONObject;
+  nIndex: Integer;
+  sValue: string;
+  nValueInteger: Double;
+begin
+  Self.ValidToJson;
+
+  oJSON := TJSONObject.Create;
+  try
+    for nIndex := ZeroValue to Pred(Self.Count) do
+    begin
+      sValue := Self.ValueFromIndex[nIndex];
+      if (sValue.ToLower.Trim = 'null') then
+      begin
+        oJSON.AddPair(Self.Names[nIndex],TJSONNull.Create);
+      end
+      else if (sValue.ToLower.Trim = 'true') then
+      begin
+        oJSON.AddPair(Self.Names[nIndex],TJSONTrue.Create);
+      end
+      else if (sValue.ToLower.Trim = 'false') then
+      begin
+        oJSON.AddPair(Self.Names[nIndex],TJSONFalse.Create);
+      end
+      else
+      begin
+        try
+          nValueInteger := StrToFloat(sValue);
+          oJSON.AddPair(Self.Names[nIndex],TJSONNumber.Create(nValueInteger));
+          continue;
+        except
+        end;
+
+        oJSON.AddPair(Self.Names[nIndex],Self.ValueFromIndex[nIndex]);
+      end;
+    end;
+
+    Result := oJSON.ToString;
+  finally
+    oJSON.Free
+  end;
+end;
+
+end.
---
+++ territory/delphi/squire/mission12/mission12.dpr
@@ -0,0 +1,76 @@
+//////////////////////////////////////////////////////////////
+//  Author: Messias Bueno                                   //
+//    Date: 06/10/2019                                      //
+// Mission: Criar um Class Helper para a classe TStringList //
+//          com as seguintes funções:                       //
+//            IsEmpty: Verifica se lista está vazia;        //
+//            HasDuplicate: Verifica se a lista contém      //
+//              valores duplicados;                         //
+//            ToJSON: Verifica se o conteúdo da Lista está  //
+//              no formato Nome=Valor. Caso esteja,         //
+//              transforme o conteúdo conforme a notação    //
+//              JSON e retorne como string (não é necessário//
+//              utilizar as classes nativas neste momento). //
+//              Caso não esteja, lance uma exceção          //
+//              informando que não foi possível converter o //
+//              conteúdo.                                   //
+//////////////////////////////////////////////////////////////
+program mission12;
+
+{$APPTYPE CONSOLE}
+
+{$R *.res}
+
+uses
+  System.SysUtils,
+  System.Classes,
+  System.Classes.Helper in 'System.Classes.Helper.pas';
+
+procedure main;
+var
+  oList: TStringList;
+begin
+  oList := TStringList.Create;
+  try
+    try
+      oList.toJson;
+    except
+      on E: Exception do
+        Writeln(e.Message);
+    end;
+
+    try
+      oList.Add('duplicated');
+      oList.Add('duplicated');
+      oList.toJson;
+    except
+      on E: Exception do
+        Writeln(e.Message);
+    end;
+
+    try
+      oList.Clear;
+      oList.Add('Invalid Item');
+      oList.toJson;
+    except
+      on E: Exception do
+        Writeln(e.Message);
+    end;
+
+    oList.Clear;
+    oList.Values['Code'] := 10.ToString;
+    oList.Values['Name'] := 'Messias';
+    oList.Values['Active'] := 'True';
+    oList.Values['City'] := 'null';
+
+    Writeln(oList.toJson);
+  finally
+    oList.Free;
+  end;
+
+  Readln;
+end;
+
+begin
+  main;
+end.
